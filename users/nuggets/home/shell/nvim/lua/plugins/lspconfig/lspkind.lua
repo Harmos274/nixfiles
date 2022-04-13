4e69a9149f4adf0ceeeeefe968f983d690af2c34
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-04-13 21:34:25 +0100

    Update Nvim config

--- users/nuggets/home/shell/nvim/lua/plugins/lspconfig/lspkind.lua
+++ users/nuggets/home/shell/nvim/lua/plugins/lspconfig/lspkind.lua
@@ -1,41 +1,41 @@
 return function ()
   require('lspkind').init({
-    -- enables text annotations
-    --
-    -- default: true
-    with_text = true,
+    -- defines how annotations are shown
+    -- default: symbol
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+    mode = 'text_symbol',
 
     -- default symbol map
-    -- can be either 'default' or
-    -- 'codicons' for codicon preset (requires vscode-codicons font installed)
+    -- can be either 'default' (requires nerd-fonts font) or
+    -- 'codicons' for codicon preset (requires vscode-codicons font)
     --
     -- default: 'default'
-    -- preset = 'codicons',
+    preset = 'default',
 
     -- override preset symbols
     --
     -- default: {}
-    symbol_map = {
-      Text = '',
-      Method = 'ƒ',
-      Function = '',
-      Constructor = '',
-      Variable = '',
-      Class = '',
-      Interface = 'ﰮ',
-      Module = '',
-      Property = '',
-      Unit = '',
-      Value = '',
-      Enum = '了',
-      Keyword = '',
-      Snippet = '﬌',
-      Color = '',
-      File = '',
-      Folder = '',
-      EnumMember = '',
-      Constant = '',
-      Struct = ''
-    },
+    -- symbol_map = {
+    --   Text = '',
+    --   Method = 'ƒ',
+    --   Function = '',
+    --   Constructor = '',
+    --   Variable = '',
+    --   Class = '',
+    --   Interface = 'ﰮ',
+    --   Module = '',
+    --   Property = '',
+    --   Unit = '',
+    --   Value = '',
+    --   Enum = '了',
+    --   Keyword = '',
+    --   Snippet = '﬌',
+    --   Color = '',
+    --   File = '',
+    --   Folder = '',
+    --   EnumMember = '',
+    --   Constant = '',
+    --   Struct = ''
+    -- },
   })
 end
--- users/nuggets/home/shell/nvim/lua/plugins/nvimtree.lua
+++ users/nuggets/home/shell/nvim/lua/plugins/nvimtree.lua
@@ -1,14 +1,14 @@
 return function ()
-  local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+  -- local tree_cb = require'nvim-tree.config'.nvim_tree_callback
   local map = require('utils').map
 
   require('nvim-tree').setup {
     open_on_setup = true,
     ignore_ft_on_setup = {},
-    auto_close = true,
+    --auto_close = true,
     update_cwd = true,
 
-    update_to_buf_dir   = {
+    update_to_buf_dir = {
       -- enable the feature
       enable = true,
       -- allow to open the tree if it was previously closed
@@ -18,10 +18,10 @@ return function ()
     diagnostics = {
       enable = true,
       icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
+        hint = "❓",
+        info = "🔎",
+        warning = "😠",
+        error = "🥲",
       }
     },
 
@@ -50,40 +50,9 @@ return function ()
         custom_only = false,
         -- list of mappings to set on the tree manually
         list = {
-          { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
-          { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
-          { key = "<C-v>",                        cb = tree_cb("vsplit") },
-          { key = "<C-x>",                        cb = tree_cb("split") },
-          { key = "<C-t>",                        cb = tree_cb("tabnew") },
-          { key = "<",                            cb = tree_cb("prev_sibling") },
-          { key = ">",                            cb = tree_cb("next_sibling") },
-          { key = "P",                            cb = tree_cb("parent_node") },
-          { key = "<BS>",                         cb = tree_cb("close_node") },
-          { key = "<S-CR>",                       cb = tree_cb("close_node") },
-          { key = "<Tab>",                        cb = tree_cb("preview") },
-          { key = "K",                            cb = tree_cb("first_sibling") },
-          { key = "J",                            cb = tree_cb("last_sibling") },
-          { key = "I",                            cb = tree_cb("toggle_ignored") },
-          { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-          { key = "R",                            cb = tree_cb("refresh") },
-          { key = "a",                            cb = tree_cb("create") },
-          { key = "d",                            cb = tree_cb("remove") },
-          { key = "r",                            cb = tree_cb("rename") },
-          { key = "<C-r>",                        cb = tree_cb("full_rename") },
-          { key = "x",                            cb = tree_cb("cut") },
-          { key = "c",                            cb = tree_cb("copy") },
-          { key = "p",                            cb = tree_cb("paste") },
-          { key = "y",                            cb = tree_cb("copy_name") },
-          { key = "Y",                            cb = tree_cb("copy_path") },
-          { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-          { key = "[c",                           cb = tree_cb("prev_git_item") },
-          { key = "]c",                           cb = tree_cb("next_git_item") },
-          { key = "-",                            cb = tree_cb("dir_up") },
-          { key = "s",                            cb = tree_cb("system_open") },
-          { key = "q",                            cb = tree_cb("close") },
-          { key = "g?",                           cb = tree_cb("toggle_help")
+            { key = "<C-k>",                        action = "" },
+            { key = "<M-k>",                        action = "toggle_file_info" },
         }
-      }
     }
   },
 }
